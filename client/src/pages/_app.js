788cb3ddfc11dc94fe18144aa3a741a49ab6d598
Raef-Hany <98807473+Raef-Hany@users.noreply.github.com>
2023-10-31 12:33:38 +0200

    added homepage

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import AppContextProvider from "@/context/AppContextProvider";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "../theme";
+import React from "react";
+import Link from "next/link";
+import { UserContext } from "@/context/UserContext";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,3 +16,31 @@ export default function App({ Component, pageProps }) {
     </ThemeProvider>
   );
 }
+
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/groups">
+              <a>Groups</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/songs">
+              <a>Songs</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+
